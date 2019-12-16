302dcca54c902c132125d59f42539df013943f10
Jochem Baas <jochemjuriaan.baas@coderclass.nl>
2019-12-16 11:33:50 +0100

    [change] better, safer api token

--- src/components/main/Github.js
+++ src/components/main/Github.js
@@ -16,7 +16,7 @@ class Github extends Component {
         fetch("https://api.github.com/users/jochem33/received_events", { 
             method: 'get', 
             headers: new Headers({
-              'Authorization': 'Basic '+btoa('jochem33:f551e7465389336f8579a88352ee719a8e275d2d'), 
+              'Authorization': 'Basic '+btoa('jochem33: fff859384e5b180705280d5961c1bff585d91c08'), 
               'Content-Type': 'application/x-www-form-urlencoded'
             })
         })
@@ -36,7 +36,7 @@ class Github extends Component {
           <div className="dashboardComponent gridItemGithub">
                 <h1>Github</h1>
                 {this.state.content}
-                <Person />
+                {/* <Person /> */}
           </div>
       )
     }
