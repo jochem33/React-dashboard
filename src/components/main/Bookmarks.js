5bf9adca0b27e97c2570eb18559310a01dd4e5ed
Jochem Baas <jochemjuriaan.baas@coderclass.nl>
2020-01-26 12:34:48 +0100

    [change] bookmarks is now a functional component

--- src/components/main/Bookmarks.js
+++ src/components/main/Bookmarks.js
@@ -2,29 +2,20 @@ import React, {Component} from "react"
 
 import bookmarksData from "../../data/bookmarksData"
 
-class Bookmarks extends Component {
-    constructor() {
-        super()
-        this.state = {
-        }
-    }
+function Bookmarks() {
+    let bookmarks = bookmarksData.map((bookmark, index) => {
+        return(
+               <a className="bookmarkLink" key={index} href={bookmark.url}>{bookmark.text}</a>
+            )
+    })
 
-    
-    render() {
-        let bookmarks = bookmarksData.map((bookmark, index) => {
-            return(
-                   <a className="bookmarkLink" key={index} href={bookmark.url}>{bookmark.text}</a>
-                )
-        })
-
-        return (
-          <div className="dashboardComponent gridItemBookmarks">
-                <h1>Bookmarks</h1>
-                <br/>
-                {bookmarks}
-          </div>
-      )
-    }
+    return (
+      <div className="dashboardComponent gridItemBookmarks">
+            <h1>Bookmarks</h1>
+            <br/>
+            {bookmarks}
+      </div>
+  )
 }
 
 export default Bookmarks
